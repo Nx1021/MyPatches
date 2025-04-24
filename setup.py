1908df3fea1c658b4c8764ca0e728a7a59d9b836
Nx1021 <571697388@qq.com>
2025-04-24 17:33:58 +0800

    0.1

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mypatches",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["matplotlib"],  # 填写依赖项（如 "numpy>=1.20"）
+)
